c7c96c70a434d3347aeb2b923a293e37df2364a6
NohaAshraf85 <noha_ashraf85@hotmail.com>
2021-04-22 11:56:22 -0400

    created the index.js that creates the relations between the models that initialize the tables in the db

--- models/index.js
+++ models/index.js
@@ -5,12 +5,35 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  as: "category_product"
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE',
+  as: 'catergory_product'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: "tags"
+})
 
 // Tags belongToMany Products (through ProductTag)
+TimeRanges.belongsToMany(Products. {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  as: 'products'
+})
 
 module.exports = {
   Product,
