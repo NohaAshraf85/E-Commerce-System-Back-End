3e555f3f48a1a0ffa463a6172b070cac5b65b953
NohaAshraf85 <noha_ashraf85@hotmail.com>
2021-04-22 21:38:22 -0400

    fixed syntax errors

--- models/index.js
+++ models/index.js
@@ -6,15 +6,13 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: 'category_id',
-  as: "category_product"
+  foreignKey: 'category_id'
 });
 
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: 'product_id',
-  onDelete: 'CASCADE',
-  as: 'catergory_product'
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
 });
 
 // Products belongToMany Tags (through ProductTag)
